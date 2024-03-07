c42555339e07ec81b46303f3b6de38e303c910cb
Vikash Kumar Pandey <112485773+Vikash2984@users.noreply.github.com>
2024-03-07 17:34:37 +0530

    Update List Comprehension.py

--- List Comprehension.py
+++ List Comprehension.py
@@ -29,3 +29,36 @@ print (list2)
 # list comprehension
 list2 = [x for x in list1 if (x % 2 == 0)]
 print (list2)
+
+
+#Flattening a list of lists
+list1 = [[11, 22, 33], [44, 55, 66], [77, 88, 99]]
+list2 = []
+for temp_list in list1:
+    for x in temp_list:
+        list2.append(x)
+print (list2)
+# list comprehension
+list2 = [x for temp_list in list1 for x in temp_list]
+print (list2)
+
+
+
+#Enlisting max() of max list
+list1 = [[11,22,33],[33,44,55],[66,77,88]]
+final_list = []
+for temp_list in list1:
+    final_list.append(max(temp_list))
+print(final_list)
+
+o = [max(temp) for temp in list1]
+print(o)
+
+
+
+#Enlisting max() of sum list
+list1 = [[11,22,33],[33,44,55],[66,77,88]]
+final_list = []
+for n in range(len(list1)):
+    list2 = [sum(x) for x in list1]
+print(list2)
