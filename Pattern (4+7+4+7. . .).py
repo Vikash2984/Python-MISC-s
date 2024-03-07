dea139718872bc3cee3a2564ee697b2b66fd7208
Vikash Kumar Pandey <112485773+Vikash2984@users.noreply.github.com>
2024-03-07 17:20:19 +0530

    Create Pattern (4+7+4+7. . .).py

---
+++ Pattern (4+7+4+7. . .).py
@@ -0,0 +1,10 @@
+n = int(input("Please enter the number of numbers: "))
+term = 4
+total = 0
+for i in range(n):
+    total = total + term
+    print (f"For i = {i} term = {term}...")
+    # term = ???   assignment statement
+
+    term = 7 if (i%2 == 0) else 4
+print (f"So for n = {n} total = {total}...")
