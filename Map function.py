54593bbae86dd35b52e260b9cf1aa23389af6e7f
Vikash Kumar Pandey <112485773+Vikash2984@users.noreply.github.com>
2024-03-07 17:05:39 +0530

    Create Map function.py

---
+++ Map function.py
@@ -0,0 +1,32 @@
+def myfunct(mystr):
+    return len(mystr)
+list1 = ['Sunday', 'Tuesday', 'Thursday', 'Saturday']
+output = map(myfunct, list1)
+opl = list(output)
+print(list1)
+print(output)
+print(list(output))
+print(opl[0])
+
+
+
+
+def myfunct2(var1,var2):
+    return var1 + var2
+lsit1 = ["Vikash", "Sudhanshu", "Adnan"]
+list2 = ["Pandey", "Singh","Shahadat"]
+o = map(myfunct2,lsit1,list2)
+print(list(o))
+
+
+
+
+def myfunct3(val):
+    val.split()
+i = input("Enter values : ")
+temp = myfunct3(i)
+o = map(split,temp)
+print(list(o))
+
+output = list(map(int,input("Enter : ").split()))
+print(output)
