0f56fffb50e9cc078b91056a1c0c0871fac316c4
Vikash Kumar Pandey <112485773+Vikash2984@users.noreply.github.com>
2024-03-07 17:20:46 +0530

    Update Pattern (4+7+4+7. . .).py

--- Pattern (4+7+4+7. . .).py
+++ Pattern (4+7+4+7. . .).py
@@ -5,6 +5,5 @@ for i in range(n):
     total = total + term
     print (f"For i = {i} term = {term}...")
     # term = ???   assignment statement
-
     term = 7 if (i%2 == 0) else 4
 print (f"So for n = {n} total = {total}...")
